e0570ee3a12d533481b736d32204ae89fa0164a8
Alexey Kozadaev <alexey.kozadaev@softezza.ru>
2025-10-06 00:51:03 +0300

    добавляет код с транзакцией

---
+++ 03_transactions/README.md
@@ -0,0 +1,4 @@
+Использование транзакций
+
+Установка драйвера
+go get github.com/mattn/go-sqlite3
---
+++ 03_transactions/main.go
@@ -0,0 +1,41 @@
+// transaction_example.go
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./test.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tx.Rollback() // откат при панике или ошибке
+
+	_, err = tx.Exec("INSERT INTO users (name, age) VALUES (?, ?)", "Bob", 28)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = tx.Exec("INSERT INTO users (name, age) VALUES (?, ?)", "Charlie", 35)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Транзакция успешно завершена")
+}
